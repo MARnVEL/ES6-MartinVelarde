e852744daef6df693f47ed6ccaf4abcbfeb76e4b
ricardom.velarde@gmail.com <ricardom.velarde@gmail.com>
2022-09-05 07:08:54 -0300

    Cargué el modulo node-fetch de npm 05-09-2022 07:08hs

--- libs/funciones-exportadas.js
+++ libs/funciones-exportadas.js
@@ -1,5 +1,5 @@
 
-
+const fetch = require('node-fetch');
 const retrasar = milisegundos => new Promise(resolve => setTimeout(resolve, milisegundos));
 
 // Función que retorna los datos de provincias
